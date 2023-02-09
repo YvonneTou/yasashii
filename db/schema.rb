abdd5fc978e4b1dcda4bd0093f76577793729295
Danielle Matsumoto <danielle.a.smart@gmail.com>
2023-02-09 21:02:41 +0900

    connections model

--- db/schema.rb
+++ db/schema.rb
@@ -10,8 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-
-ActiveRecord::Schema[7.0].define(version: 2023_02_09_112301) do
+ActiveRecord::Schema[7.0].define(version: 2023_02_09_115956) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -27,6 +26,21 @@ ActiveRecord::Schema[7.0].define(version: 2023_02_09_112301) do
     t.datetime "updated_at", null: false
   end
 
+  create_table "connections", force: :cascade do |t|
+    t.bigint "user_id", null: false
+    t.bigint "clinic_id", null: false
+    t.datetime "start_time"
+    t.datetime "end_time"
+    t.datetime "appt_date"
+    t.integer "status", default: 0
+    t.string "symptoms"
+    t.text "info"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["clinic_id"], name: "index_connections_on_clinic_id"
+    t.index ["user_id"], name: "index_connections_on_user_id"
+  end
+
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
     t.string "encrypted_password", default: "", null: false
@@ -40,4 +54,6 @@ ActiveRecord::Schema[7.0].define(version: 2023_02_09_112301) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
+  add_foreign_key "connections", "clinics"
+  add_foreign_key "connections", "users"
 end
