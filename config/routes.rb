63ad9ac8c9045cd1153c278b1d2fbedf25482a64
Tanner Maxwell <tanner.ray.maxwell@gmail.com>
2023-02-09 20:29:08 +0900

    added dashboard policy

--- config/routes.rb
+++ config/routes.rb
@@ -6,5 +6,5 @@ Rails.application.routes.draw do
   # Defines the root path route ("/")
   # root "articles#index"
 
-  get "/dashboard", to: "dashboard#dashboard", as: dashboard
+  get "/dashboard", to: "dashboard#dashboard", as: :dashboard
 end
