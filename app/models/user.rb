ccab7fc77a7f797a444cfcaa1242156118a0a387
Yvonne Tou <113744446+YvonneTou@users.noreply.github.com>
2023-02-09 21:49:46 +0900

    Update user.rb
    added the has_many connection relationship responding to the connection model

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,7 @@
 class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
+  has_many :connections
   has_many :messages, as: :sender
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
