88f992c58c52c6dc1085ee32d343219248b28fa1
Tanner Maxwell <tanner.ray.maxwell@gmail.com>
2023-02-11 11:21:16 +0900

    remove specialty column from clinics table

--- db/schema.rb
+++ db/schema.rb
@@ -10,14 +10,12 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-
-ActiveRecord::Schema[7.0].define(version: 2023_02_09_120358) do
+ActiveRecord::Schema[7.0].define(version: 2023_02_11_021943) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
   create_table "clinics", force: :cascade do |t|
     t.string "name"
-    t.string "specialty"
     t.string "location"
     t.string "hours"
     t.string "phone_number"
