5f604f7f6ab05cc821f2c1beb766e881c8f2c77f
Tanner Maxwell <tanner.ray.maxwell@gmail.com>
2023-02-11 14:12:46 +0900

    wrote basic logic for connection seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,6 +6,7 @@
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
 puts "Destroying existing records..."
+Connection.destroy_all
 User.destroy_all
 puts "Done deletion"
 
@@ -17,3 +18,31 @@ User.create(username: "Dani", email: "danielle@email.com", password: "123456")
 User.create(username: "Eevie", email: "yvonne@email.com", password: "123456")
 
 puts "Done creating users"
+
+puts "Creating 8 connections..."
+
+User.all.each do |user|
+    Connection.create!({
+      user: user
+      clinic: Clinic.all.sample
+      start_time:
+      end_time:
+      appointment_date:
+      symptoms: [ SYMPTOMS.sample, SYMPTOMS.sample ]
+      info: "I require wheelchair access."
+      status:
+    })
+
+    Connection.create!({
+      user: user
+      clinic: Clinic.all.sample
+      start_time:
+      end_time:
+      appointment_date:
+      symptoms: [ SYMPTOMS.sample, SYMPTOMS.sample ]
+      info: INFO.sample
+      status:
+    })
+end
+
+puts "Done creating connections"
