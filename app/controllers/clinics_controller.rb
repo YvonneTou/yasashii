668f9a966fd7cd69f6652f304197a0f5c56424bc
Danielle Matsumoto <danielle.a.smart@gmail.com>
2023-02-11 16:45:00 +0900

    dashboard view added

--- app/controllers/clinics_controller.rb
+++ app/controllers/clinics_controller.rb
@@ -2,10 +2,10 @@ class ClinicsController < ApplicationController
   # access specialities via `specialty_list: []`
 
   def index
-    # @clinics = Clinic.all
+    @clinics = Clinic.all
   end
 
   def show
-    # @clinic = Clinic.find(params[:id])
+    @clinic = Clinic.find(params[:id])
   end
 end
--- app/policies/dashboard_policy.rb
+++ app/policies/dashboard_policy.rb
@@ -1,7 +1,15 @@
 class DashboardPolicy < ApplicationPolicy
   attr_reader :current_user, :document
 
+  class Scope < Scope
+    # NOTE: Be explicit about which records you allow access to!
+    def resolve
+      scope.all
+    end
+  end
+
   def dashboard?
+    true
   end
 
 end
