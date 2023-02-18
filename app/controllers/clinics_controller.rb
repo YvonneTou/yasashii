fca958075ebb91959cb2b76c1cbfe8edbd95295f
Sarah Rollins <s.rollins751@gmail.com>
2023-02-18 14:15:09 +0900

    added basic location search function

--- app/controllers/clinics_controller.rb
+++ app/controllers/clinics_controller.rb
@@ -1,9 +1,14 @@
 class ClinicsController < ApplicationController
   skip_before_action :authenticate_user!
+  skip_after_action :verify_policy_scoped, only: [:index]
   # access specialities via `specialty_list: []`
 
   def index
-    @clinics = policy_scope(Clinic)
+    if params[:query].present?
+      @clinics = Clinic.search_by_keyword(params[:query])
+    else
+      @clinics = Clinic.all
+    end
   end
 
   def show
