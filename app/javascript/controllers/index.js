576c12afffc52542e3e5e0522af1733c97103412
Danielle Matsumoto <danielle.a.smart@gmail.com>
2023-02-25 17:58:52 +0900

    toggle symptoms

--- app/javascript/controllers/index.js
+++ app/javascript/controllers/index.js
@@ -9,3 +9,6 @@ application.register("hello", HelloController)
 
 import MapController from "./map_controller"
 application.register("map", MapController)
+
+import SymptomController from "./symptom_controller"
+application.register("symptom", SymptomController)
