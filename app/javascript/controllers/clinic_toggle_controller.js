23342966b6547b4dfe9225a0a5e009b8f625a686
Danielle Matsumoto <danielle.a.smart@gmail.com>
2023-03-04 16:08:50 +0900

    toggle about and booking started

--- app/javascript/controllers/clinic_toggle_controller.js
+++ app/javascript/controllers/clinic_toggle_controller.js
@@ -2,6 +2,10 @@ import { Controller } from "@hotwired/stimulus"
 
 // Connects to data-controller="clinic-toggle"
 export default class extends Controller {
+  static targets = ["navbar"]
+
   connect() {
   }
+
+
 }
