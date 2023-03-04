ecc156b91bd0036c3285f7bc73d1954bf7d1280b
Sarah Rollins <s.rollins751@gmail.com>
2023-03-04 14:56:51 +0900

    limited recently visited clinics to 5

--- app/models/clinic.rb
+++ app/models/clinic.rb
@@ -3,7 +3,7 @@ class Clinic < ApplicationRecord
   has_many :messages, as: :sender
   has_one_attached :photo
   acts_as_taggable_on :specialties
-  geocoded_by :location
+  geocoded_by :location, params: { country: "jp", proximity: "139.817413,35.652832" }
   after_validation :geocode, if: :will_save_change_to_location?
 
   include PgSearch::Model
