d72c5f4a7d99099c24c54efccc3b6ea9b7bb3177
Yvonne Tou <tou.yvonne@gmail.com>
2023-03-09 00:40:39 +0900

    update about page style, add characatch banner as placeholder for our group photo later

--- app/controllers/about_controller.rb
+++ app/controllers/about_controller.rb
@@ -3,5 +3,41 @@ class AboutController < ApplicationController
   after_action :verify_authorized, except: [:show]
 
   def show
+    @members = User.first(4)
+    @contact_info = contact_info
+  end
+
+  private
+
+  def contact_info
+    {
+      yvonne: {
+        role: 'Lead Developer',
+        gmail: 'tou.yvonne@gmail.com',
+        github: 'https://github.com/YvonneTou/',
+        linkedin: 'https://www.linkedin.com/in/touyvonne/'
+      },
+
+      tanner: {
+        role: 'Backend Lead',
+        gmail: 'tanner.ray.maxwell@gmail.com',
+        github: 'https://github.com/tmxds',
+        linkedin: 'https://www.linkedin.com/in/???/'
+      },
+
+      sarah: {
+        role: 'Frontend Lead',
+        gmail: 's.rollins751@gmail.com',
+        github: 'https://github.com/srollins01',
+        linkedin: 'https://www.linkedin.com/in/touyvonne/'
+      },
+
+      danielle: {
+        role: 'Project Manager',
+        gmail: 'danielle.a.smart@gmail.com',
+        github: 'https://github.com/sumaatsu',
+        linkedin: 'https://www.linkedin.com/in/???/'
+      }
+    }
   end
 end
